6449d6962450db6250aec74ed9cc8038a0593581
spica8080 <spica8080@gmail.com>
2017-05-04 11:47:32 -0500

    blog post component finished

--- app/scripts/components/app_root.js
+++ app/scripts/components/app_root.js
@@ -1,22 +1,16 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import container from '../containers/all.js'
+import React from "react";
+import { connect } from "react-redux";
+import container from "../containers/all.js";
+import BlogComponent from "../components/blog_component.js";
 
 const AppRoot = React.createClass({
-  render: function () {
+  render: function() {
     return (
       <section>
-        <h1>Blog</h1>
-        <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <input id="checkBox" type="checkbox"/>
+        <BlogComponent />
       </section>
-
     );
   }
 });
 
-export default connect(container.allState)(AppRoot)
+export default connect(container.allState)(AppRoot);
